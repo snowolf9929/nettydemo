47318c0a4da11a2b868b5e7954f3b140023245d5
qi_yao <qi_yao@kingdee.com>
2014-10-05 16:03:42 +0800

    分隔符解码器demo

---
+++ src/main/java/com/github/snowolf9929/nettydemo/delimiter/EchoServerHandler.java
@@ -0,0 +1,30 @@
+package com.github.snowolf9929.nettydemo.delimiter;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Created by youki on 14-10-5.
+ */
+public class EchoServerHandler extends ChannelHandlerAdapter {
+
+    int counter = 0;
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String) msg;
+        System.out.println("This is " + ++counter + " times receive client : [" + body + "]");
+        body += "$_";
+        ByteBuf echo = Unpooled.copiedBuffer(body.getBytes());
+        ctx.writeAndFlush(echo);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
